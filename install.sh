918e45d1d771c6532597510f8431c8c531817cad
Toby Jones <github@monotron.me>
2020-09-20 20:43:20 +0100

    Add install script for dotfiles

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Make .config directory in home folder if it does not exist
+[[ -d ~/.config ]] || mkdir ~/.config
+
+# Install alacritty configuration
+if [[ -f ~/.config/alacritty.yml ]]; then
+    echo "- Skipping alacritty installation as a config file already exists."
+else
+    ln -s "$(pwd)/alacritty.yml" ~/.config/
+fi
+
